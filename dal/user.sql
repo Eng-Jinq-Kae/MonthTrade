8cac73e2a79a797e71761b1b70203ba2583c226f
Eng-Jinq-Kae <jinqkae17@gmail.com>
2025-12-24 02:36:26 +0800

    dataloader user guideline

--- dal/user.sql
+++ dal/user.sql
@@ -2,13 +2,20 @@ SELECT current_user; --curent user credential
 SELECT usename FROM pg_user; --all PostgreSQL roles (users)
 --Schemas → pg_catalog → Views
 
+--Step 1
 CREATE USER mthtradeuser
 WITH PASSWORD '123';
+
+--Step 2
 GRANT CONNECT ON DATABASE "MthTrade" TO "mthtradeuser";
+
+--Step 3
 GRANT USAGE ON SCHEMA public TO "mthtradeuser";
 GRANT SELECT, INSERT, UPDATE
 ON ALL TABLES IN SCHEMA public
 TO "mthtradeuser";
+
+--Step 4
 ALTER DEFAULT PRIVILEGES
 IN SCHEMA public
 GRANT SELECT, INSERT, UPDATE
