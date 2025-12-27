b30a834346b5a8500e44cfe59482bef68374fd2d
Eng-Jinq-Kae <jinqkae17@gmail.com>
2025-12-27 14:00:07 +0800

    streamlit home link

--- pages/1_Home.py
+++ pages/1_Home.py
@@ -1,3 +1,23 @@
 import streamlit as st
 
-st.title("Home")
+st.title("Home: SITC Monthly Trade")
+st.page_link(
+    "app.py",
+    label="Go to app Page to know about SITC",
+    icon=":material/settings:"
+)
+st.page_link(
+    "pages/2_Table.py",
+    label="Go to Table Page",
+    icon=":material/table:"
+)
+st.page_link(
+    "pages/3_Prediction Moving Average.py",
+    label="Go to Moving Average Page",
+    icon=":material/acute:"
+)
+st.page_link(
+    "pages/4_Prediction Linear Regression.py",
+    label="Go to Linear Regression Page",
+    icon=":material/bolt:"
+)
