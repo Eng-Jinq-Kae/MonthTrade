3975d05972827f20fd032cc93a223d9b6fdd5c97
Eng-Jinq-Kae <jinqkae17@gmail.com>
2025-12-27 13:17:50 +0800

    streamlit table all header

--- pages/2_Table.py
+++ pages/2_Table.py
@@ -2,13 +2,15 @@ import streamlit as st
 import dataloader as dl
 
 st.title("Table")
-df = dl.read_ref_section()
-section_list = df['Section'].unique().tolist()
+df_ref = dl.read_ref_section()
+df_db = dl.read_data_monthtrade()
+section_list = df_ref['Section'].unique().tolist()
 for section in section_list:
-    section_name = df.loc[df['Section'] == section, 'SectionName'].iloc[0]
+    section_name = df_ref.loc[df_ref['Section'] == section, 'SectionName'].iloc[0]
     st.subheader(f'{section} - {section_name}')
-    df_section = dl.read_data_monthtrade_section(section)
-    st.dataframe(df_section, hide_index=True)
+    df_section = df_db[df_db['Section'] == section]
+    df_section_show = df_section.rename(columns={"Exports": "Exports (RM)", "Imports": "Imports (RM)"})
+    st.dataframe(df_section_show, hide_index=True)
     df_plot = df_section.set_index('Date')[['Exports', 'Imports']]
     st.line_chart(df_plot, color=('#FF7F0E','#1F77B4'))
     st.write(f'Chart of: {section_name}')
