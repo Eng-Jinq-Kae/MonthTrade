20f4fe8a8511d7a99e6a996d5c40c7cb3acebc55
Eng-Jinq-Kae <jinqkae17@gmail.com>
2025-12-28 17:27:17 +0800

    st1 container

--- pages/1_Home.py
+++ pages/1_Home.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import dataloader as dl
 
 st.title("Home: SITC Monthly Trade")
 st.page_link(
@@ -21,3 +22,28 @@ st.page_link(
     label="Go to Linear Regression Page",
     icon=":material/bolt:"
 )
+
+df_ref = dl.read_ref_section()
+df_actual = dl.update_data_mthtrade_db()
+
+def write_container(section: int, df_actual, df_ref):
+    container = st.container(border=True)
+
+    with container:
+        st.write(f"Section {section}")
+        # section_name = df_ref.loc[df_ref['Section'] == section, 'SectionName'].iloc[0]
+        # st.write(section_name)
+
+
+section = 'Overall'
+st.subheader('Overall')
+write_container(section, df_actual, df_ref)
+
+st.subheader('by Section')
+row1 = st.columns(5)
+row2 = st.columns(5)
+all_cols = row1 + row2
+for section, col in enumerate(all_cols):
+    section = str(section)
+    with col:
+        write_container(section, df_actual, df_ref)
